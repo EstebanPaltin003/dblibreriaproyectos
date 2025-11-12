2648bd6a1efea8bda53ffe3a798a8772241433e3
User <ppaltin@tecnologicoismac.edu.ec>
2025-11-12 11:53:03 -0500

    New project uwu

---
+++ src/main/java/com/distribuida/model/FacturaDetalle.java
@@ -0,0 +1,58 @@
+package com.distribuida.model;
+
+public class FacturaDetalle {
+
+    // ATRIBUTOS
+    private int idDetalle;
+    private Libro libro;
+    private int cantidad;
+
+    // CONSTRUCTORES
+    public FacturaDetalle() { }
+
+    public FacturaDetalle(int idDetalle, Libro libro, int cantidad) {
+        this.idDetalle = idDetalle;
+        this.libro = libro;
+        this.cantidad = cantidad;
+    }
+
+    // GETTERS Y SETTERS
+    public int getIdDetalle() {
+        return idDetalle;
+    }
+
+    public void setIdDetalle(int idDetalle) {
+        this.idDetalle = idDetalle;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    //metodo de calculo
+    public double calcularSubtotal() {
+        return libro.getPrecio() * cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "FacturaDetalle{" +
+                "idDetalle=" + idDetalle +
+                ", libro=" + libro.getTitulo() +
+                ", cantidad=" + cantidad +
+                ", subtotal=" + calcularSubtotal() +
+                '}';
+    }
+}
